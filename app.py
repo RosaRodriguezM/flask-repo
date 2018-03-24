f2d0be8dbf55de79533a4722443d0919e782d321
Rosa Rodriguez <rosa.rodriguez.mota@mg.thedataincubator.com>
2018-03-24 16:22:05 +0000

    Second commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.Stock = GOOG
+app.Type = Open
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    app.Stock = request.form['Ticker']
+    app.Type = request.form['Type']
+    f = open('answers.txt')
+    f.write('Stock: %s\n',%app.Stock)
+    f.write('Type: %s\n',%app.Type)
+    f.close()
+    return redirect('/graph')
+
+@app.route('/graph')
+def graph():
+  return render_template('about.html')
 
 @app.route('/about')
 def about():
