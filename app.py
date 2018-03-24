495b1808eb2251c5b207be2a960a095de35bf920
Rosa Rodriguez <rosa.rodriguez.mota@mg.thedataincubator.com>
2018-03-24 22:29:49 +0000

    No se cual sea el problema commit

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
   else:
     app.Stock = request.form['Stock']
     app.Type = request.form['Select']
+    f = open('answers.txt')
+    f.write('Stock: %s\n'%(app.Stock))
+    f.write('Type: %s\n'%(app.Type))
+    f.close()
     return redirect('/about')
 
 
