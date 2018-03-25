563bb4f36463438bfdb28eef41cd46eae69e83f8
Rosa Rodriguez <rosa.rodriguez.mota@mg.thedataincubator.com>
2018-03-25 17:35:33 +0000

    No se cual sea el problema commit

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def graph():
   raw_data = session.get(api_url)
   R=loads(raw_data.content)
   DATA=DataFrame(R['data'],columns=R['column_names']) 
-  p = figure(title='Data from Quandle WIKI set', x_axis_label='Date', x_axis_type='datetime')
+  p = figure(title='Data from Quandle WIKI set', x_axis_label='%s'%app.Type, x_axis_type='datetime')
   p.line(to_datetime(DATA['Date']),DATA['Open'], color= Spectral11[0],line_width=1)
   script, div = components(p)
   return render_template('graph.html', script=script, div=div) 
